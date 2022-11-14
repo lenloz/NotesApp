1712a654c0259dab5f10ecd83305b35eafbbf994
Lenin Lozada <51455382+lenloz@users.noreply.github.com>
2022-11-14 17:24:23 -0400

    Update app.js
    borrar cors y utilizar el paquete helmet

--- server/app.js
+++ server/app.js
@@ -3,13 +3,13 @@ const path = require("path");
 const connectToDatabase = require("./database/dbConfig");
 const noteRoutes = require("./routes/index");
 const errorMiddleware = require("./middlewares/error");
-const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(helmet());
 
-app.use(cors());
 
 app.use(noteRoutes);
 
